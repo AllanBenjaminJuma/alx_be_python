7bb1e84355518562ab41f27be279362693146123
allan_Ben <allanbenjuma@gmail.com>
2025-06-01 22:50:53 +0300

    Removed spaces within print in multiplication_table.py

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -2,5 +2,5 @@ number = int(input("Enter a number to see its multiplication table:"))
 
 for Y in range(1,11):
     Z = number * Y
-    print(number, " ", "*", " ", Y," ","=", Z)
+    print(number, "*", Y,"=",Z)
     
