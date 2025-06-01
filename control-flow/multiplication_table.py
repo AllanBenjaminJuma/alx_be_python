ee7e46595f16585a0c41de42d69f4dfb86d87f3e
allan_Ben <allanbenjuma@gmail.com>
2025-06-01 22:48:53 +0300

    Corrected the variable number in multiplication_table.py

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -1,6 +1,6 @@
-X = int(input("Enter a number to see its multiplication table:"))
+number = int(input("Enter a number to see its multiplication table:"))
 
 for Y in range(1,11):
-    Z = X * Y
-    print(X, " ", "*", " ", Y," ","=", Z)
+    Z = number * Y
+    print(number, " ", "*", " ", Y," ","=", Z)
     
